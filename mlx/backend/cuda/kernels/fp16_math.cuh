ml-explore/mlx

CUDA backend: reduce ops

mlx/backend/cuda/kernels/fp16_math.cuh
@@ -9,6 +9,58 @@
 namespace mlx::core::cu {
 
 ///////////////////////////////////////////////////////////////////////////////
+// Constant values for half types.
+///////////////////////////////////////////////////////////////////////////////
+
+#define MLX_DEFINE_CONSTEXPR_VALUE(NAME, HALF_VALUE, BF16_VALUE, ...) \
+  template <typename T>                                               \
+  constexpr __host__ __device__ T NAME() {                            \
+    if constexpr (cuda::std::is_same_v<T, __half>) {                  \
+      uint16_t value = HALF_VALUE;                                    \
+      return __builtin_bit_cast(__half, value);                       \
+    } else if constexpr (cuda::std::is_same_v<T, __nv_bfloat16>) {    \
+      uint16_t value = BF16_VALUE;                                    \
+      return __builtin_bit_cast(__nv_bfloat16, value);                \
+    } else {                                                          \
+      __VA_ARGS__                                                     \
+    }                                                                 \
+  }
+
+MLX_DEFINE_CONSTEXPR_VALUE(zero_value, 0x0000, 0x0000, {
+  if constexpr (cuda::std::is_same_v<T, cuComplex>) {
+    return cuComplex{0, 0};
+  } else {
+    return 0;
+  }
+})
+
+MLX_DEFINE_CONSTEXPR_VALUE(one_value, 0x3C00, 0x3F80, {
+  if constexpr (cuda::std::is_same_v<T, cuComplex>) {
+    return cuComplex{1, 1};
+  } else {
+    return 1;
+  }
+})
+
+MLX_DEFINE_CONSTEXPR_VALUE(infinite_value, 0x7C00, 0x7F80, {
+  return cuda::std::numeric_limits<T>::infinity();
+})
+
+MLX_DEFINE_CONSTEXPR_VALUE(negative_infinite_value, 0xFC00, 0xFF80, {
+  return -cuda::std::numeric_limits<T>::infinity();
+})
+
+MLX_DEFINE_CONSTEXPR_VALUE(finite_max_value, 0x7BFF, 0x7F7F, {
+  return cuda::std::numeric_limits<T>::max();
+})
+
+MLX_DEFINE_CONSTEXPR_VALUE(finite_min_value, 0xFBFF, 0xFF7F, {
+  return cuda::std::numeric_limits<T>::min();
+})
+
+#undef MLX_DEFINE_CONSTEXPR_VALUE
+
+///////////////////////////////////////////////////////////////////////////////
 // Unary ops for half types.
 ///////////////////////////////////////////////////////////////////////////////
 

mlx/backend/cuda/kernels/utils.cuh
@@ -35,6 +35,55 @@ inline cuda::std::array<T, MAX_NDIM> const_param(const std::vector<T>& vec) {
 }
 
 ///////////////////////////////////////////////////////////////////////////////
+// Type limits utils
+///////////////////////////////////////////////////////////////////////////////
+
+template <typename U>
+struct Limits {
+  static constexpr U max = cuda::std::numeric_limits<U>::max();
+  static constexpr U min = cuda::std::numeric_limits<U>::min();
+  static constexpr U finite_max = cuda::std::numeric_limits<U>::max();
+  static constexpr U finite_min = cuda::std::numeric_limits<U>::min();
+};
+
+template <>
+struct Limits<bool> {
+  static constexpr bool max = true;
+  static constexpr bool min = false;
+};
+
+template <>
+struct Limits<cuComplex> {
+  static constexpr cuComplex max = {
+      cuda::std::numeric_limits<float>::infinity(),
+      cuda::std::numeric_limits<float>::infinity()};
+  static constexpr cuComplex min = {
+      -cuda::std::numeric_limits<float>::infinity(),
+      -cuda::std::numeric_limits<float>::infinity()};
+};
+
+// Like MLX_FORALL_FLOAT_TYPES but use CUDA types.
+#define MLX_FORALL_CUDA_FLOAT_TYPES(_) \
+  _(float, float32)                    \
+  _(double, float64)                   \
+  _(__half, float16)                   \
+  _(__nv_bfloat16, bfloat16)
+
+// Some CCCL/CUDA combinations do not provide constexpr limits for half types.
+#define SPECIALIZE_FloatLimits(CPP_TYPE, DTYPE)                          \
+  template <>                                                            \
+  struct Limits<CPP_TYPE> {                                              \
+    static constexpr CPP_TYPE max = infinite_value<CPP_TYPE>();          \
+    static constexpr CPP_TYPE min = negative_infinite_value<CPP_TYPE>(); \
+    static constexpr CPP_TYPE finite_max = finite_max_value<CPP_TYPE>(); \
+    static constexpr CPP_TYPE finite_min = finite_min_value<CPP_TYPE>(); \
+  };
+
+MLX_FORALL_CUDA_FLOAT_TYPES(SPECIALIZE_FloatLimits)
+
+#undef SPECIALIZE_FloatLimits
+
+///////////////////////////////////////////////////////////////////////////////
 // Indexing utils
 ///////////////////////////////////////////////////////////////////////////////
 

frost-beta/mlx-cuda

mlx/backend/cuda/kernels/fp16_math.cuh
@@ -9,6 +9,58 @@
 namespace mlx::core::cu {
 
 ///////////////////////////////////////////////////////////////////////////////
+// Constant values for half types.
+///////////////////////////////////////////////////////////////////////////////
+
+#define MLX_DEFINE_CONSTEXPR_VALUE(NAME, HALF_VALUE, BF16_VALUE, ...) \
+  template <typename T>                                               \
+  constexpr __host__ __device__ T NAME() {                            \
+    if constexpr (cuda::std::is_same_v<T, __half>) {                  \
+      uint16_t value = HALF_VALUE;                                    \
+      return __builtin_bit_cast(__half, value);                       \
+    } else if constexpr (cuda::std::is_same_v<T, __nv_bfloat16>) {    \
+      uint16_t value = BF16_VALUE;                                    \
+      return __builtin_bit_cast(__nv_bfloat16, value);                \
+    } else {                                                          \
+      __VA_ARGS__                                                     \
+    }                                                                 \
+  }
+
+MLX_DEFINE_CONSTEXPR_VALUE(zero_value, 0x0000, 0x0000, {
+  if constexpr (cuda::std::is_same_v<T, cuComplex>) {
+    return cuComplex{0, 0};
+  } else {
+    return 0;
+  }
+})
+
+MLX_DEFINE_CONSTEXPR_VALUE(one_value, 0x3C00, 0x3F80, {
+  if constexpr (cuda::std::is_same_v<T, cuComplex>) {
+    return cuComplex{1, 1};
+  } else {
+    return 1;
+  }
+})
+
+MLX_DEFINE_CONSTEXPR_VALUE(infinite_value, 0x7C00, 0x7F80, {
+  return cuda::std::numeric_limits<T>::infinity();
+})
+
+MLX_DEFINE_CONSTEXPR_VALUE(negative_infinite_value, 0xFC00, 0xFF80, {
+  return -cuda::std::numeric_limits<T>::infinity();
+})
+
+MLX_DEFINE_CONSTEXPR_VALUE(max_value, 0x7BFF, 0x7F7F, {
+  return cuda::std::numeric_limits<T>::max();
+})
+
+MLX_DEFINE_CONSTEXPR_VALUE(lowest_value, 0xFBFF, 0xFF7F, {
+  return cuda::std::numeric_limits<T>::lowest();
+})
+
+#undef MLX_DEFINE_CONSTEXPR_VALUE
+
+///////////////////////////////////////////////////////////////////////////////
 // Unary ops for half types.
 ///////////////////////////////////////////////////////////////////////////////
 

mlx/backend/cuda/kernels/utils.cuh
@@ -2,6 +2,8 @@
 
 #pragma once
 
+#include "mlx/backend/cuda/kernels/fp16_math.cuh"
+
 #include <cuComplex.h>
 #include <cuda/std/array>
 #include <cuda/std/limits>
@@ -29,6 +31,55 @@ inline cuda::std::array<T, MAX_NDIM> const_param(const std::vector<T>& vec) {
   std::copy_n(vec.begin(), vec.size(), result.begin());
   return result;
 }
+
+///////////////////////////////////////////////////////////////////////////////
+// Type limits utils
+///////////////////////////////////////////////////////////////////////////////
+
+template <typename U>
+struct Limits {
+  static constexpr U max = cuda::std::numeric_limits<U>::max();
+  static constexpr U min = cuda::std::numeric_limits<U>::min();
+  static constexpr U finite_max = cuda::std::numeric_limits<U>::max();
+  static constexpr U finite_min = cuda::std::numeric_limits<U>::min();
+};
+
+template <>
+struct Limits<bool> {
+  static constexpr bool max = true;
+  static constexpr bool min = false;
+};
+
+template <>
+struct Limits<cuComplex> {
+  static constexpr cuComplex max = {
+      cuda::std::numeric_limits<float>::infinity(),
+      cuda::std::numeric_limits<float>::infinity()};
+  static constexpr cuComplex min = {
+      -cuda::std::numeric_limits<float>::infinity(),
+      -cuda::std::numeric_limits<float>::infinity()};
+};
+
+// Like MLX_FORALL_FLOAT_TYPES but use CUDA types.
+#define MLX_FORALL_CUDA_FLOAT_TYPES(_) \
+  _(float, float32)                    \
+  _(double, float64)                   \
+  _(__half, float16)                   \
+  _(__nv_bfloat16, bfloat16)
+
+// Some CCCL/CUDA combinations do not provide constexpr limits for half types.
+#define SPECIALIZE_FloatLimits(CPP_TYPE, DTYPE)                          \
+  template <>                                                            \
+  struct Limits<CPP_TYPE> {                                              \
+    static constexpr CPP_TYPE max = infinite_value<CPP_TYPE>();          \
+    static constexpr CPP_TYPE min = negative_infinite_value<CPP_TYPE>(); \
+    static constexpr CPP_TYPE finite_max = max_value<CPP_TYPE>();        \
+    static constexpr CPP_TYPE finite_min = lowest_value<CPP_TYPE>();     \
+  };
+
+MLX_FORALL_CUDA_FLOAT_TYPES(SPECIALIZE_FloatLimits)
+
+#undef SPECIALIZE_FloatLimits
 
 ///////////////////////////////////////////////////////////////////////////////
 // Indexing utils
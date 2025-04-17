55e4c979586c340624baa3ec9efe03e7845d1154
Cheng <git@zcbenz.com>
2025-04-17 09:13:00 +0000

    CUDA backend: reduce ops

--- mlx/backend/cuda/kernels/utils.cuh
+++ mlx/backend/cuda/kernels/utils.cuh
@@ -2,6 +2,8 @@
 
 #pragma once
 
+#include "mlx/backend/cuda/kernels/fp16_math.cuh"
+
 #include <cuComplex.h>
 #include <cuda/std/array>
 #include <cuda/std/limits>
@@ -29,6 +31,55 @@ inline cuda::std::array<T, MAX_NDIM> const_param(const std::vector<T>& vec) {
   std::copy_n(vec.begin(), vec.size(), result.begin());
   return result;
 }
+
+///////////////////////////////////////////////////////////////////////////////
+// Type limits utils
+///////////////////////////////////////////////////////////////////////////////
+
+template <typename U>
+struct Limits {
+  static constexpr U max = cuda::std::numeric_limits<U>::max();
+  static constexpr U min = cuda::std::numeric_limits<U>::min();
+  static constexpr U finite_max = cuda::std::numeric_limits<U>::max();
+  static constexpr U finite_min = cuda::std::numeric_limits<U>::min();
+};
+
+template <>
+struct Limits<bool> {
+  static constexpr bool max = true;
+  static constexpr bool min = false;
+};
+
+template <>
+struct Limits<cuComplex> {
+  static constexpr cuComplex max = {
+      cuda::std::numeric_limits<float>::infinity(),
+      cuda::std::numeric_limits<float>::infinity()};
+  static constexpr cuComplex min = {
+      -cuda::std::numeric_limits<float>::infinity(),
+      -cuda::std::numeric_limits<float>::infinity()};
+};
+
+// Like MLX_FORALL_FLOAT_TYPES but use CUDA types.
+#define MLX_FORALL_CUDA_FLOAT_TYPES(_) \
+  _(float, float32)                    \
+  _(double, float64)                   \
+  _(__half, float16)                   \
+  _(__nv_bfloat16, bfloat16)
+
+// Some CCCL/CUDA combinations do not provide constexpr limits for half types.
+#define SPECIALIZE_FloatLimits(CPP_TYPE, DTYPE)                          \
+  template <>                                                            \
+  struct Limits<CPP_TYPE> {                                              \
+    static constexpr CPP_TYPE max = infinite_value<CPP_TYPE>();          \
+    static constexpr CPP_TYPE min = negative_infinite_value<CPP_TYPE>(); \
+    static constexpr CPP_TYPE finite_max = max_value<CPP_TYPE>();        \
+    static constexpr CPP_TYPE finite_min = lowest_value<CPP_TYPE>();     \
+  };
+
+MLX_FORALL_CUDA_FLOAT_TYPES(SPECIALIZE_FloatLimits)
+
+#undef SPECIALIZE_FloatLimits
 
 ///////////////////////////////////////////////////////////////////////////////
 // Indexing utils
